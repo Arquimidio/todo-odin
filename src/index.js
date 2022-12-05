65d91684fe428ccec596390ab8aaf1d1e0796cea
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-12-05 19:58:11 -0300

    Start every module from scratch

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Storage from "./components/Storage";
-import Project from "./components/Project";
-import Task from "./components/Task"
-
-Storage.initStorage();
-Storage.add(new Project('ElPires'));
-Storage.add(new Task('lol', '2022-12-05', 3, 'Raw data', false), 'ElPires');
